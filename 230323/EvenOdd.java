eaced90c1244601e2e36eff8dab45541a0e8f129
Rashmi Howal <114422521+HowalRashmi@users.noreply.github.com>
2023-03-24 10:54:11 +0530

    Create EvenOdd.java

---
+++ 230323/EvenOdd.java
@@ -0,0 +1,16 @@
+//write a java program to find if number is even or odd
+class EvenOdd    //creating class
+{
+	public static void main(String args[])   //main method
+	{
+		int a=15;   //declaring variable
+		if (a%2==0)  //if condition to check if no is even
+		{
+			System.out.println(a+" is an even Number");   //printing output
+		}
+		else
+		{
+			System.out.println(a+" is a odd Number");    //printing output
+		}
+	}
+}
