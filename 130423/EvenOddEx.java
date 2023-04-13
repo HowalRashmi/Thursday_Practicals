c927ad2ae5349f51ba45f91c4e04ce8a2a2da840
Rashmi Howal <114422521+HowalRashmi@users.noreply.github.com>
2023-04-13 14:06:09 +0530

    Create EvenOddEx.java

---
+++ 130423/EvenOddEx.java
@@ -0,0 +1,31 @@
+/*  Write a program to create thread. Where you will take the input from the user and find if
+the number is odd or even.*/
+package practicals130423;
+
+import java.util.Scanner;               //importing scanner
+
+class EvenOdd extends Thread           //creating threads
+{
+	public void run()                    //run method for thread
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter no to check if it is even or odd: ");
+		int i= sc.nextInt();                        //getting input from user
+		if(i%2==0)                                  //checking for even or odd
+		{
+			System.out.println("Given number is even");
+		}
+		else
+		{
+			System.out.println("Given number is odd");
+		}
+	}
+}
+public class EvenOddEx {           
+
+	public static void main(String[] args) {             //main method
+		EvenOdd eo=new EvenOdd();                  //creating new thread
+		eo.start();                                //start thread
+	}
+
+}
