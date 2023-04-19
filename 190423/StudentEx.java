aacf8e6d641478909462e75dcdef515b9792d5c3
Rashmi Howal <114422521+HowalRashmi@users.noreply.github.com>
2023-04-19 14:06:35 +0530

    Create StudentEx.java

---
+++ 190423/StudentEx.java
@@ -0,0 +1,31 @@
+/* Write a program to print the names of students by creating a Student class. If no 
+ name is passed while creating an object of Student class, then the name should be
+ "Unknown", otherwise the name should be equal to the String value passed while creating 
+ object of Student class.*/
+package practicals130423;
+
+import java.util.Scanner;
+
+class Student
+{
+	String name;
+	Student()
+	{
+		this.name="Unknown";
+		System.out.println(name);
+	}
+	Student(String name)
+	{
+		this.name=name;
+		System.out.println(name);
+	}
+}
+public class StudentEx {
+
+	
+	public static void main(String[] args) {
+		Student s1= new Student();
+		Student s2= new Student("ABC");
+	}
+
+}
